5bb24900f2835b799a05e38201ffdfca270c9cb1
Pranava <pranavankl@gmail.com>
2023-12-16 16:56:58 +0530

    lang translation added

--- newsgatherers/urls.py
+++ newsgatherers/urls.py
@@ -7,18 +7,17 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/',views.logout,name='logout'),
     path('admin_dashboard/',views.admin_dashboard,name='admin_dashboard'),
-  
-    path('article/<int:index>/<int:id>',views.article,name='article'),
+    path('cluster/<int:id>',views.cluster,name='cluster'),
+    path('article/',views.article,name='article'),
     path('youtube_videos/',views.youtube_data_home,name='youtube_data_home'),
     path('youtube_video_analysis/<int:id>',views.youtube_data_analysis,name='youtube_data_analysis'),
     path('text_video/',views.text_video,name='text_video'),
     # path('eprints',views.eprints,name='eprints'),
-#     path('eprint',views.eprint,name='eprint'),
+    path('eprint',views.eprint,name='eprint'),
     path('newsanalysis',views.newsanalysis,name='newsanalysis'),
-#     path('youtubes',views.youtubes,name='youtubes'),
+    # path('youtubes',views.youtubes,name='youtubes'),
     # path('youtubes',views.youtubes,name='youtubes'),
     path('dash',views.dash,name='dash'),
     # path('filters',views.filters,name='filters'),
     path('lang',views.lang,name='lang'),
-    
 ]
